a4a59ab98b80fcc74bb921467c811ffa73dd983e
Charmant27 <charmantogatera@gmail.com>
2024-03-30 13:22:17 +0200

    Added constant values

--- constants/index.ts
+++ constants/index.ts
@@ -1,33 +1,56 @@
-// <block:setup:1>
-const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
-  };
-  // </block:setup>
-  
-  // <block:config:0>
-  const config = {
-    type: 'doughnut',
-    data: data,
-  };
-  // </block:config>
-  
-  module.exports = {
-    actions: [],
-    config: config,
-  };
+export const data = [
+  {
+    id: 1,
+    position: 'Senior data analyst',
+    positions_left: 3,
+    applications: 123,
+    interviewed: 40,
+    rejected: 33,
+    feedback_pending: 7,
+    offered: 2,
+  },
 
-  export default data
+  {
+    id: 2,
+    position: 'Junior data analyst',
+    positions_left: 7,
+    applications: 567,
+    interviewed: 22,
+    rejected: 20,
+    feedback_pending: 2,
+    offered: 4,
+  },
+
+  {
+    id: 3,
+    position: 'Product designer',
+    positions_left: 2,
+    applications: 201,
+    interviewed: 32,
+    rejected: 18,
+    feedback_pending: 14,
+    offered: 0,
+  },
+
+  {
+    id: 4,
+    position: 'Java developer',
+    positions_left: 5,
+    applications: 231,
+    interviewed: 23,
+    rejected: 10,
+    feedback_pending: 13,
+    offered: 3,
+  },
+
+  {
+    id: 5,
+    position: 'Product Manager',
+    positions_left: 3,
+    applications: 67,
+    interviewed: 41,
+    rejected: 22,
+    feedback_pending: 19,
+    offered: 1,
+  },
+]
